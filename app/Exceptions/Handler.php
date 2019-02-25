423263f27f2c76601321db117f46d181ee873d50
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2019-02-25 08:01:33 -0600

    Render JSON error for invalid HTTP method

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json([
+                'message' => 'Invalid HTTP method.'
+            ], 405);
+        }
         return parent::render($request, $exception);
     }
 }
